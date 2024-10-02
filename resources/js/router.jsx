2d3737b87a61cbeff7b7f0e9494fa1d1e0276020
SarahThabitt <sarah.thabit@thetranslationgate.com>
2024-10-02 13:13:18 +0300

    fix route file

--- resources/js/router.jsx
+++ resources/js/router.jsx
@@ -15,6 +15,13 @@ import PosList from "./pages/VM/POs&Invoices/PosList";
 // import AllRunningCpos from "./pages/VM/POs&Invoices/All_Running_Cpos";
 // import PaymentMethods from "./pages/VM/POs&Invoices/Payment_Methods";
 
+import Portal_Dashboard from "./pages/VendorPortal/Dashboard";
+import Portal_Jobs_All from "./pages/VendorPortal/Jobs/AllJobs";
+import Portal_Jobs_Closed from "./pages/VendorPortal/Jobs/ClosedJobs";
+import Portal_Jobs_Offers from "./pages/VendorPortal/Jobs/Offers";
+import Portal_Jobs_Notifications from "./pages/VendorPortal/Jobs/Notifications";
+import Portal_Invoices_All from "./pages/VendorPortal/Invoices/AllInvoices";
+import Portal_Invoices_Verified from "./pages/VendorPortal/Invoices/VerifiedInvoices";
 
 
 
@@ -46,19 +53,53 @@ const router = createBrowserRouter([
             
         ]
     },
-    {
-        path: '/Vendor',
-        element: <Dashboard_p />,
-        children: [
+         {
+        path:'/Vendor',
+        element:<Dashboard_p/>,
+        children:[        
             {
-                path: '',
-                element: <Home />
+                path:'',
+                element:<Home/>
+            },      
+            {              
+                path:'Jobs',              
+                children:[
+                    {   path:'',
+                        element:<Portal_Jobs_All/>}
+                    ,
+                    {
+                        path:'Offers',
+                        element:<Portal_Jobs_Offers/>
+                    },
+                    {
+                        path:'Closed',
+                        element:<Portal_Jobs_Closed/>
+                    },      
+                    {
+                        path:'Notifications',
+                        element:<Portal_Jobs_Notifications/>
+                    },            
+                ]
             },
             {
-                path: 'about',
-                element: <About />
+              
+                path:'Invoices',              
+                children:[
+                    {   path:'',
+                        element:<Portal_Invoices_All/>
+                    }
+                    ,
+                    {
+                        path:'Verified',
+                        element:<Portal_Invoices_Verified/>
+                    },
+                                                                                                                                                                ]
             },
-
+            {
+                path:'Admin',
+                element:<About/>
+            },      
+                       
         ]
     }
 
