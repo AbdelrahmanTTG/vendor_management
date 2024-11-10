a25aba213749ad16c5188db38c1c83ec5fcd8596
Mohamed Elghamry <mohamed_e20@hotmail.com>
2024-11-10 04:17:39 +0200

    trying

--- resources/js/pages/VM/VendorManagement/CodeTable/index.jsx
+++ resources/js/pages/VM/VendorManagement/CodeTable/index.jsx
@@ -1,13 +1,36 @@
 import React, { Fragment, useContext } from 'react';
 import Table from './table';
 // import Search from './LanguagesSearch';
+// const CodeTables = React.memo((props) => {
+//     return (
+//         <React.Fragment>
+//             {/* <Search /> */}
+//             <Table table={props.table} dataTable={props.dataTable} header={props.header} fields={props.fields} related={props.related} columns={props.columns} />
+//         </React.Fragment>
+//     );
+// });
+// This component receives props from a parent component and passes them to the Table component
 const CodeTables = React.memo((props) => {
+    // Destructure props to make them easier to work with
+    const { table, dataTable, header, fields, related, columns } = props;
+
+    // Memoize the props to avoid unnecessary re-renders in the Table component
+    const tableProps = useMemo(() => ({
+        table,
+        dataTable,
+        header,
+        fields,
+        related,
+        columns
+    }), [table, dataTable, header, fields, related, columns]);
+
+    // Render the component
     return (
         <Fragment>
+            {/* Optional Search component can be included here */}
             {/* <Search /> */}
-            <Table table={props.table} dataTable={props.dataTable} header={props.header} fields={props.fields} related={props.related} columns={props.columns} />
+            <Table {...tableProps} />
         </Fragment>
     );
 });
-
 export default CodeTables;
