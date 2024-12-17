bafae931905c430120e4172f7498129b095d1db1
AbdelrahmanTTG <abdelrahman.khaled@thetranslationgate.com>
2024-12-17 17:16:19 +0200

    test

--- resources/js/real-time.jsx
+++ resources/js/real-time.jsx
@@ -6,10 +6,10 @@ window.Pusher = Pusher;
 const createEcho = () => {
     return new Echo({
         broadcaster: 'reverb',
-        key: import.meta.env.VITE_REVERB_APP_KEY ?? "qm42aq7xixjvpowejavl",
-        wsHost: import.meta.env.VITE_REVERB_HOST ?? "dev.aixnexus.com",
-        wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        key:"qm42aq7xixjvpowejavl",
+        wsHost:"127.0.0.1",
+        wsPort:  6001,
+        wssPort:  443,
         forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
         enabledTransports: ['ws', 'wss'],
         auth: {
