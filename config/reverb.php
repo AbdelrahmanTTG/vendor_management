d46e89a582eaef3f4f3232d045222b465f846bb2
AbdelrahmanTTG <abdelrahman.khaled@thetranslationgate.com>
2025-02-12 18:52:18 +0200

    test

--- config/reverb.php
+++ config/reverb.php
@@ -120,7 +120,7 @@ return [
 
         'reverb' => [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 6001),
+            'port' => env('REVERB_SERVER_PORT', 443),
             'hostname' => env('REVERB_HOST', "portal.lingotalents.com"),
             'options' => [
                 'tls' => [],
