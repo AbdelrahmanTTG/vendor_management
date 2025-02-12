239c396fdb3e92cef5ce4200b02367496d3c0c9c
AbdelrahmanTTG <abdelrahman.khaled@thetranslationgate.com>
2025-02-12 16:40:43 +0200

    test

--- resources/js/real-time.jsx
+++ resources/js/real-time.jsx
@@ -8,8 +8,8 @@ const createEcho = () => {
         broadcaster: 'reverb',
         key: "qm42aq7xixjvpowejavl",
         wsHost:"stage.aixnexus.com",
-        wsPort: 6001,
-        wssPort:  6001,
+        wsPort:6001,
+        wssPort:6001,
         forceTLS: true,
         enabledTransports: ['ws', 'wss'],
         auth: {
