c10a3fa722c4f71b762f9f7c6deb456814953625
AbdelrahmanTTG <abdelrahman.khaled@thetranslationgate.com>
2025-02-13 13:13:28 +0200

    test

--- app/Events/Message.php
+++ app/Events/Message.php
@@ -24,10 +24,8 @@ class Message implements ShouldBroadcastNow
 
     public function broadcastOn()
     {
-        return [
-            new PrivateChannel('newMessage-private-channel.User.' . $this->userId)
-        ];
-        // return new PrivateChannel('newMessage-private-channel.User.' . $this->userId);
+        
+        return new PrivateChannel('newMessage-private-channel.User.' . $this->userId);
         //  return [
         //         new Channel('test'),
         //  ] ;
