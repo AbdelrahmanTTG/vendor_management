0c2a6fff6cb2680bb4d65ea83c490b0bb34625d6
AbdelrahmanTTG <abdelrahman.khaled@thetranslationgate.com>
2025-11-11 13:56:46 +0200

    update crypto

--- resources/js/crypto.js
+++ resources/js/crypto.js
@@ -1,24 +1,12 @@
 import CryptoJS from "crypto-js";
 
-const secretKey = window.APP_KEY;
-let key = CryptoJS.SHA256(secretKey); 
-let iv = CryptoJS.enc.Hex.parse(key.toString().substr(0, 32));
-
+const SALT = window.APP_KEY;
 export const encryptData = (data) => {
     const stringData = JSON.stringify(data);
-    const encrypted = CryptoJS.AES.encrypt(stringData, key, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-    });
-    return encrypted.toString(); 
+    return CryptoJS.AES.encrypt(stringData, SALT).toString();
 };
-
 export const decryptData = (cipherText) => {
-    const decrypted = CryptoJS.AES.decrypt(cipherText, key, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-    });
-    return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
+    const bytes = CryptoJS.AES.decrypt(cipherText, SALT);
+    const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+    return JSON.parse(decrypted);
 };
